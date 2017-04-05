16401469b806635adc553efc3e0b208e49afae3c
sovorent <bj.sunzz@gmail.com>
2017-04-05 11:13:42 +0700

    api version 1.1

--- api/models/todoListModels.js
+++ api/models/todoListModels.js
@@ -1,1 +1,28 @@
 
+'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+
+var TaskSchema = new Schema({
+  name: {
+    type: String,
+    Required: 'Kindly enter the name of the task'
+  },
+  contents: {
+    type: String
+  },
+  Created_date: {
+    type: Date,
+    default: Date.now
+  },
+  status: {
+    type: [{
+      type: String,
+      enum: ['pending', 'done']
+    }],
+    default: ['pending']
+  }
+});
+
+module.exports = mongoose.model('Tasks', TaskSchema);
--- server.js
+++ server.js
@@ -1,1 +1,39 @@
 
+var express = require('express'),
+	app = express(),
+	port = process.env.PORT || 3000;
+  	mongoose = require('mongoose'),
+  	Task = require('./api/models/todoListModels'),
+  	bodyParser = require('body-parser');
+
+  	mongoose.Promise = global.Promise;
+  	var dbURI = 'mongodb://localhost/Tododb';
+  	mongoose.connect(dbURI);
+  	// CONNECTION EVENTS
+	// When successfully connected
+	mongoose.connection.on('connected', function () {  
+	  console.log('Mongoose default connection open to ' + dbURI);
+	}); 					
+	// If the connection throws an error
+	mongoose.connection.on('error',function (err) {  
+	  console.log('Mongoose default connection error: ' + err);
+	}); 
+
+  	app.use(bodyParser.urlencoded({ extended: true }));
+  	app.use(bodyParser.json());
+
+  	var routes = require('./api/routes/todoListRoutes');
+	routes(app);
+
+	app.use(function(req, res) {
+	  res.status(404).send({url: req.originalUrl + ' not found'})
+	});
+
+
+
+
+///////////////////////////////////////
+app.listen(port,function(){
+	console.log('API server started on port : ' + port);
+});
+
